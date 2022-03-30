5c680bbe0852e1b254dd5fa0cafff3df5cfeec8e
18JOyiki <92166950+18JOyiki@users.noreply.github.com>
2022-03-30 14:42:55 +0100

    Update q1.py

--- test1/q1.py
+++ test1/q1.py
@@ -1,6 +1,6 @@
 n1 = float(input())
 n2 = float(input())
 avg = (n1 + n2) / 2
-print(f"Remainder of {n1} divided by {n2} is {n1 % n2}")
-print(f"Remainder of {n2} divided by {n1} is {n2 % n1}")
+print(f"Remainder of {n1} divided by {n2} is {n2 % n1}")
+print(f"Remainder of {n2} divided by {n1} is {n1 % n2}")
 print(f"{n1}^n{2}={n1**n2}")
